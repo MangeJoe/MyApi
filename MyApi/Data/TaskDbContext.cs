f6811e1b95976310303b9d1dca8ec3f4894c9e9e
Mange Joe <madzira3@gmail.com>
2026-04-18 12:12:38 +0200

    Add project files.

---
+++ MyApi/Data/TaskDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyApi.Domain.Entities;
+
+namespace MyApi.Data
+{
+    public class TaskDbContext :DbContext
+    {
+        public TaskDbContext(DbContextOptions options) : base(options)
+        { 
+        }
+
+        public DbSet<TodoTask> Tasks { get; set; }
+    }
+}
---
+++ MyApi/Domain/Entities/TodoTask.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyApi.Domain.Entities
+{
+    [Table("task")]
+    public record TodoTask
+    {
+            [Key]
+            [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+            [Column("id")]
+            public int Id { get; set; }
+
+            [Column("title")]
+            [MaxLength(100)]
+            public string Title { get; set; }
+
+            [Column("description")]
+            [MaxLength(500)]
+            public string Description { get; set; }
+
+            [Column("duedate")]
+            public DateTime DueDate { get; set; }
+
+            [Column("status")]
+            public int Status { get; set; }
+
+            [Column("priority")]
+            public int Priority { get; set; }
+        
+    }
+}
