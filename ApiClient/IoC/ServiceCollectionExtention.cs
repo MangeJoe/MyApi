1c22790cf31a0300452601233380ced93ad3686e
Mange Joe <madzira3@gmail.com>
2026-04-21 16:05:37 +0200

    Added

---
+++ ApiClient/IoC/ServiceCollectionExtention.cs
@@ -0,0 +1,24 @@
+﻿using ApiClient.Models;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiClient.IoC
+{
+    public static class ServiceCollectionExtention
+    {
+        public static void AddApiClientService(this IServiceCollection services,Action<ApiClientOptions> options)
+        {
+            services.Configure(options);
+            services.AddSingleton(provider=>
+            {
+                var options=provider.GetRequiredService<IOptions<ApiClientOptions>>().Value;
+                return new ApiClientService(options);
+            });
+        }
+    }
+}
