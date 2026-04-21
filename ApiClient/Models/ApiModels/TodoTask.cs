59121d5347c522f08fc2cae3450d73c2c93a0810
Mange Joe <madzira3@gmail.com>
2026-04-21 16:05:53 +0200

    added model

---
+++ ApiClient/Models/ApiModels/TodoTask.cs
@@ -0,0 +1,13 @@
+﻿
+namespace ApiClient.Models.ApiModels
+{
+    public class TodoTask
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public int Status { get; set; }
+        public int Priority { get; set; }
+    }
+}
